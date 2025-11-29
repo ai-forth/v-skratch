d40426e2517d590ec3fc8a5d71504ad290a5a27e
C.A.Tucker <cartheur@pm.me>
2025-11-29 16:41:15 +0100

    Mix it up

--- tests/pcm-analyze.f
+++ tests/pcm-analyze.f
@@ -26,7 +26,7 @@ s" test.raw"  2constant TEST-FILE    \ file you want to scan
 1 FRAC-BITS lshift constant SCALE     \ SCALE = 1 << FRAC-BITS (16384)
 
 \ Scaled detection threshold (0.80 → 0.80 * SCALE)
-0.80e0 FRAC-BITS lshift f>s constant THRESHOLD-FIXED
+0.80e0 SCALE s>f f* f>s constant THRESHOLD-FIXED
 
 cr ." FRAC-BITS = " FRAC-BITS . cr
 cr ." SCALE     = " SCALE . cr
