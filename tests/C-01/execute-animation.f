03f9d1f7dd96f42a47dc53b70b70f57c6d56c161
C.A.Tucker <cartheur@pm.me>
2025-12-01 16:57:34 +0100

    Pseudocode

--- tests/C-01/execute-animation.f
+++ tests/C-01/execute-animation.f
@@ -1,3 +1,12 @@
 \ =====================================================================
 \  Behaviour Routine: Execute one of a number of possible animations
 \  --------------------------------------------------------------------
+\ The choices are:
+\ ---------------------------------------------
+\ Spin clockwise/counterclockwise based on mood
+
+
+\ Dance (need motor routine)
+
+
+\ Acknowledge (sound emanation)
