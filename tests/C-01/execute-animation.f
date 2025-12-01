041d42e97bd57f7dc642cd716dfa6da570046ea1
C.A.Tucker <cartheur@pm.me>
2025-12-01 18:24:12 +0100

    Expand pseudocode

--- tests/C-01/execute-animation.f
+++ tests/C-01/execute-animation.f
@@ -1,12 +1,20 @@
 \ =====================================================================
-\  Behaviour Routine: Execute one of a number of possible animations
+\  Behaviour Routine: Execute one of a number of possible animations.
+\   Make the code such that each component is demarcated as a 'module' that can be interchanged.
 \  --------------------------------------------------------------------
 \ The choices are:
 \ ---------------------------------------------
-\ Spin clockwise/counterclockwise based on mood
+\ Indicate Mood Interaction
+\ if (happy)    - spin clockwise +1 rotation [ 1-motor CW; 2-motor CCW ] | How to determine that enough rot for returning to facing the user?
+\ if (pensive)  - move (forward) +1 rot, move (backward) -1 rot
+\ if (...)
+\ else          - spin counterclockwise  -1 rotation [ 1-motor CCW; 2-motor CW ] | Same concern here as the one above.
 
+\ Dance
+\  Sounds and movements
+\   -   mixture of move and spin
+\   -   lights and sounds
 
-\ Dance (need motor routine)
-
-
-\ Acknowledge (sound emanation)
+\ Acknowledge
+\  Sounds
+\   - tonal language
